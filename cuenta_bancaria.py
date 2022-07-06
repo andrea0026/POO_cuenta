620f7ad0b228f91bba6b9ec5557dd3308018afe6
Andrea Arboleda <andrea0026@hotmail.com>
2022-07-05 22:23:15 -0500

    correccion de return

--- cuenta_bancaria.py
+++ cuenta_bancaria.py
@@ -4,7 +4,7 @@ class CuentaBancaria:
     tasa_int = 0.01
 
     def __init__(self, tasa_int, balance): 
-        self.tasa_int = tasa_int
+        self.tasa_int = 0.5
         self.balance = balance
 
     def deposito(self, amount):
@@ -13,17 +13,18 @@ class CuentaBancaria:
 
     def retiro(self, amount):
         if CuentaBancaria.puede_retirar(self.balance,amount):
-            self.balance -= amount - 5
+            self.balance -= amount + 5
         else:
             print("Fondos insuficientes")
-            return self
+        return self
 
     def mostrar_info_cuenta(self):
-        print("Balance: " + self.balance)
+        print("Balance:", self.balance)
         return self
 
     def generar_interes(self):
-        self.balance = CuentaBancaria.calcularInteres(self.balance)
+        if self.balance > 0:
+            self.balance = self.balance*self.tasa_int
         return self
 
     @staticmethod
@@ -33,14 +34,8 @@ class CuentaBancaria:
         else:
             return True
 
-    @staticmethod
-    def calcularInteres(balance):
-        if balance > 0:
-            balance += balance*0.01
+cuenta1 = CuentaBancaria(1.003,300)
+cuenta2 = CuentaBancaria(1.003,500)
 
-
-cuenta1 = CuentaBancaria(0.01,300)
-cuenta2 = CuentaBancaria(0.01,500)
-
-cuenta1.deposito(50).deposito(40).deposito(30).retiro(100).generar_interes(0.01).mostrar_info_cuenta()
-cuenta2.deposito(100).deposito(30).retiro(100).retiro(20).retiro(20).retiro(20).generar_interes(0.01).mostrar_info_cuenta()
+cuenta1.deposito(50).deposito(40).deposito(30).retiro(100).generar_interes().mostrar_info_cuenta()
+cuenta2.deposito(100).deposito(30).retiro(100).retiro(20).retiro(10).retiro(15).generar_interes().mostrar_info_cuenta()
